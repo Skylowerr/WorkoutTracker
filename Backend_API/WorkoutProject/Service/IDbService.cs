69fabc36af61eda3aafbabefa699f62707bdd5c7
Emirhan Gökçe <58822314+Skylowerr@users.noreply.github.com>
2026-01-05 11:52:30 +0100

    Update IDbService.cs

--- Backend_API/WorkoutProject/Service/IDbService.cs
+++ Backend_API/WorkoutProject/Service/IDbService.cs
@@ -2,6 +2,7 @@ namespace WorkoutTracker.Interfaces
 {
     public interface IDbService
     {
+        //Interfaces (Contract) & Generics 
         Task<List<T>> GetAll<T>() where T : class;
         Task<T?> Get<T>(int id) where T : class;
         // Eksik olan CRUD metotlarını ekliyoruz:
