7e13dccebcc95efdd616a80dbd1f4569457b7664
Emirhan Gökçe <58822314+Skylowerr@users.noreply.github.com>
2026-01-05 11:52:43 +0100

    Update IDbService.cs

--- Backend_API/WorkoutProject/Service/IDbService.cs
+++ Backend_API/WorkoutProject/Service/IDbService.cs
@@ -5,7 +5,6 @@ namespace WorkoutTracker.Interfaces
         //Interfaces (Contract) & Generics 
         Task<List<T>> GetAll<T>() where T : class;
         Task<T?> Get<T>(int id) where T : class;
-        // Eksik olan CRUD metotlarını ekliyoruz:
         Task<T> AddAsync<T>(T entity) where T : class;
         Task UpdateAsync<T>(T entity) where T : class;
         Task DeleteAsync<T>(int id) where T : class;
